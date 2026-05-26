846c996938115d4d20cd8c6bb3352e7e325c0d38
kmardiso <kmardiso@gmail.com>
2026-05-26 07:25:19 +0700

    fix:perbaiki next.cinfig.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
     ],
@@ -8,3 +9,4 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
